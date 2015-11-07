b57460c04da4e762c4f55112004e2a042609623b
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-11-07 11:16:53 +0530

    Rename dbcontext file and added foreign key attribute

--- EFWithTransactions/EFWithTransactions/Product.cs
+++ EFWithTransactions/EFWithTransactions/Product.cs
@@ -1,9 +1,12 @@
-﻿namespace EFWithTransactions
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFWithTransactions
 {
     public class Product
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
+        [ForeignKey("Category")]
         public int CategoryId { get; set; }
     }
 }
--- EFWithTransactions/EFWithTransactions/ProductDbContext.cs
+++ EFWithTransactions/EFWithTransactions/ProductDbContext.cs
@@ -4,6 +4,11 @@ namespace EFWithTransactions
 {
     public class ProductDbContext : DbContext
     {
+        public ProductDbContext()
+            : base("ProductConnectionString")
+        {
+
+        }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
     }
