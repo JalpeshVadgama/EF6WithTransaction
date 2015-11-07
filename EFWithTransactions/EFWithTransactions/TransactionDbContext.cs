bf280cb0a53f0f9c6ebfca6442c2fcb094296a86
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-11-07 11:02:28 +0530

    Initial commit with Entity framework and few classes

---
+++ EFWithTransactions/EFWithTransactions/TransactionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace EFWithTransactions
+{
+    public class TransactionDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+    {
+    }
+
+    public class Department
+    {
+    }
+}
